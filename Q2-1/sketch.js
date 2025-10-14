10e801fd9bb159c9663919f3033ca3e6eb1608ec
okinu <hanabi.okinu@gmail.com>
2025-10-14 13:27:31 +0900

    Change on 10/14

--- Q2-1/sketch.js
+++ Q2-1/sketch.js
@@ -1,8 +1,14 @@
 // 小手調べ
 function setup(){
   createCanvas(100, 100);
-  noFill();
   for(let i = 0; i < 10; i++){
-    // BLANK[1] 条件分岐を使って、前半は赤、後半は青で円を描こう
+    let size = (i + 1) * 10;
+    noFill();
+    if(i < 5){
+      stroke(0, 0, 255); // 青
+    } else {
+      stroke(255, 0, 0); // 赤
+    }
+    ellipse(50, 50, size, size);
   }
 }
--- Q2-2/sketch.js
+++ Q2-2/sketch.js
@@ -5,7 +5,20 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] ヒント： rectのx座標は size * i, y座標は size * j
+      if ((i+j) % 2 != 0){
+        fill(100, 100, 100)
+        rect(size * i, size * j, size, size)
+      }
+
+      if ((j < 3) && ((i+j) % 2 != 0)) {
+        fill(255, 0, 0)
+        ellipse(size*i + size*0.5, size*j+size*0.5, size*0.9, size*0.9)
+      }
+
+      if ((j > 4) && ((i+j) % 2 != 0)) {
+        fill(0)
+        ellipse(size*i + size*0.5, size*j+size*0.5, size*0.9, size*0.9)
+      }
     }
   }
 }
